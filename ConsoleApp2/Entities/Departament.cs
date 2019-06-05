640a0cd74bf646facafbb9716c52082c29942231
David Almeida <davidsouzacontato@gmail.com>
2019-06-05 00:00:33 -0300

    complete

---
+++ ConsoleApp2/Entities/Departament.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.Entities
+{
+    class Departament
+    {
+        public string Name { get; set; }
+
+        public Departament()
+        {
+        }
+
+        public Departament(string name)
+        {
+            Name = name;
+        }
+    }
+}
