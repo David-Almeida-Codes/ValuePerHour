39bb972bd1711768a3420655bfc802cf389f548b
David Almeida <davidsouzacontato@gmail.com>
2019-10-18 03:18:10 -0300

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -47,7 +47,7 @@ namespace ConsoleApp2
             int year = int.Parse(monthAndYear.Substring(3));
             Console.WriteLine("Name: " + worker.Name);
             Console.WriteLine("Departament: " + worker.Departament.Name);
-            Console.WriteLine("Income for: " + monthAndYear + ": " + worker.Income(year, month));
+            Console.WriteLine("Income for: " + monthAndYear + ": " + worker.Income(year, month), CultureInfo.InvariantCulture);
 
         }
     }
